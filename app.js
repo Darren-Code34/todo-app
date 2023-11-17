5880f7b0bd45eda80ec9578632156aed5a7e4efb
Darren Mibissa <mibissadarren@outlook.com>
2023-11-17 20:29:30 +0100

    features: add a task, delete a task, checked a task

--- app.js
+++ app.js
@@ -39,3 +39,89 @@ function changeTheme(){
     }
 
 }
+
+
+
+const todoForm = document.querySelector(".todo-form");
+const taskInput = document.querySelector("#task-input");
+const infoBar = document.querySelector(".info-bar");
+
+
+todoForm.addEventListener("submit", addTask)
+
+//add a task
+
+function addTask(e){
+    e.preventDefault()
+
+    const todo = document.createElement("div");
+    const checkbox = document.createElement("div");
+    const todoName = document.createElement("p");
+    const deleteIcon = document.createElement("img");
+
+    if(taskInput.value === ""){
+        taskInput.classList.add("error");
+        taskInput.placeholder = "Enter a new task!!!";
+        setTimeout(()=>{
+            taskInput.classList.remove("error");
+            taskInput.placeholder = "Create a new todo..."
+        },2000)
+    }
+    else{
+        todo.classList.add("todo");
+
+        checkbox.classList.add("checkbox");
+
+        todoName.textContent = taskInput.value;
+        todoName.classList.add("todo-name");
+
+        deleteIcon.src = "/images/icon-cross.svg";
+        deleteIcon.classList.add("delete-icon");
+
+        todo.appendChild(checkbox);
+        todo.appendChild(todoName);
+        todo.appendChild(deleteIcon);
+        todoList.insertBefore(todo, infoBar);
+
+        taskInput.value = "";
+
+    }
+
+    //Delete a task
+
+    const deleteIcons = document.querySelectorAll(".delete-icon");
+
+    deleteIcons.forEach(deleteIcon =>{
+        deleteIcon.addEventListener("click", deleteTask);
+
+        function deleteTask(){
+            deleteIcon.parentElement.style.display = "none";
+        }
+    })
+
+
+    //mark a task done
+
+    const checkboxes = document.querySelectorAll(".checkbox");
+
+    checkboxes.forEach(checkbox =>{
+        checkbox.addEventListener("click", checkTask);
+
+        let isChecked = false;
+
+        function checkTask(){
+            if(isChecked === false){
+                checkbox.classList.add("checked");
+                checkbox.nextSibling.style.textDecoration = "line-through";
+                isChecked = true;
+            }
+            else{
+                checkbox.classList.remove("checked");
+                checkbox.nextSibling.style.textDecoration = "none";
+                isChecked = false;
+            }
+        }
+    })
+
+}
+
